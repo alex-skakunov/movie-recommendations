46dfebccac913da8948064c4f56a41a302cb7fcb
Alexander Skakunov <alex.skakunov@gmail.com>
2018-02-20 13:37:59 +0200

    Environment setup

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -4,5 +4,26 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
 
-}
+	protected function _initRouter ()
+	{
+		$this->bootstrap ('frontcontroller');
 
+		$this->getResource ('frontcontroller')
+			->setResponse(new Zend_Controller_Response_Cli())
+			->setRouter (new Application_Router_Cli ())
+			->setRequest (new Zend_Controller_Request_Simple ());
+	}
+
+
+	protected function _initError ()
+	{
+		$frontcontroller = $this->getResource ('frontcontroller');
+
+		$error = new Zend_Controller_Plugin_ErrorHandler ();
+		$error->setErrorHandlerController ('error');
+
+		$frontcontroller->registerPlugin ($error, 100);
+
+		return $error;
+	}
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,8 @@ defined('APPLICATION_PATH')
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'testing'));
 
+require_once APPLICATION_PATH . '/../vendor/autoload.php';
+
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../library'),
